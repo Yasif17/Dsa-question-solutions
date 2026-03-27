ef7226a8e48a2cc4ce5220e5bcd4e526fe0aeca2
Yasif khan <125533092+Yasif17@users.noreply.github.com>
2026-03-28 00:52:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Pattern 1/pattern-1.java
+++ Difficulty: Basic/Pattern 1/pattern-1.java
@@ -2,8 +2,8 @@ class Solution {
 
     void printSquare(int n) {
         // code here
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
                 System.out.print("* ");
             }
             System.out.println();
