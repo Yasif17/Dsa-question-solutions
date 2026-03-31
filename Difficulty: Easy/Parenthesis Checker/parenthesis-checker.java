897f51ad9425c9db409bdc2ecb97a7b5e63d7d3e
Yasif khan <125533092+Yasif17@users.noreply.github.com>
2026-04-01 00:54:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,53 +1,28 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        // Reading total number of testcases
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            // reading the string
-            String st = sc.next();
-
-            // calling ispar method of Paranthesis class
-            // and printing "balanced" if it returns true
-            // else printing "not balanced"
-            if (new Solution().isBalanced(st) == true)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
-    static boolean isBalanced(String s) {
+    public boolean isBalanced(String s) {
         // code here
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        for(int i=0;i<s.length();i++){
-            char cur = s.charAt(i);
+        Stack<Character> stack = new Stack<>();
+        
+        for(char ch:s.toCharArray())
+        {
             
-            if(cur=='(' || cur=='[' || cur=='{'){
-                stack.push(cur);
-            }else{
-                if(stack.isEmpty()) return false;
-                char opening = stack.pop();
-                if((opening=='(' && cur==')') 
-                || (opening=='[' && cur==']') 
-                || (opening=='{' && cur=='}')) continue;
-                else return false;
-            }
+            if(ch=='(' || ch=='[' || ch=='{')
+            {
+                
+                stack.push(ch);
+                
+            }else if(ch==')' || ch==']' || ch=='}')
+                {
+                
+                    if(stack.isEmpty()) return false;
+                    
+                    char top = stack.pop();
+                    
+                    if(ch==')' && top!='(' || ch==']' && top!='[' || ch=='}' && top!='{'){
+                        return false;
+                    }
+                }
         }
-        return stack.isEmpty();
+            return stack.isEmpty();
     }
 }
