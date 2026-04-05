18aca6ddec1ad0cfbffa8e7f4a88e74cf698779b
Yasif khan <125533092+Yasif17@users.noreply.github.com>
2026-04-05 23:20:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Queue using Linked List/queue-using-linked-list.java
+++ Difficulty: Basic/Queue using Linked List/queue-using-linked-list.java
@@ -1,106 +1,50 @@
-//{ Driver Code Starts
-import java.util.*;
-class QueueNode
-{
-	int data;
-	QueueNode next;
-	QueueNode(int a)
-	{
-	    data = a;
-	    next = null;
-	}
+// Node class
+class Node {
+    int data;
+    Node next;
+
+    Node(int new_data) {
+        data = new_data;
+        next = null;
+    }
 }
 
-class GfG {
-	public static void main(String args[])
-	{
-		 Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 while(t>0)
-		 {
-			MyQueue obj = new MyQueue();
-			int Q = sc.nextInt();
-			while(Q>0)
-			{
-				int QueryType = 0;
-				QueryType = sc.nextInt();
-				if(QueryType == 1)
-				{
-					int a = sc.nextInt();
-					
-					obj.push(a);
-					
-				}else if(QueryType == 2)
-				{
-				System.out.print(obj.pop()+" ");
-				}
-				Q--;
-			}
-			System.out.println("");
-			t--;
-		 
-System.out.println("~");
-}
-	}
-}
-
-
-
-
-
-// } Driver Code Ends
-
-
-/*The structure of the node of the queue is
-class QueueNode
-{
-	int data;
-	QueueNode next;
-	QueueNode(int a)
-	{
-	    data = a;
-	    next = null;
-	}
-}*/
-
-class MyQueue
-{
-    QueueNode front, rear;
+// Queue class
+class myQueue {
     
-    //Function to push an element into the queue.
-	void push(int a)
-	{
-        // Your code here
-        QueueNode newNode = new QueueNode(a);
-        
-        if(rear==null){
-            front=rear=newNode;
-            return;
-        }
-        rear.next=newNode;
-        rear=newNode;
-        
-	}
-	
-    //Function to pop front element from the queue.
-	int pop()
-	{
-        // Your code here
-        
-        if(front==null){
-            return -1;
-        }
-        
-        int value=front.data;
-        front=front.next;
-        
-        if(front==null){
-            rear=null;
-        }
+    Node head,tail;
+    int size=0;
+    
+    public myQueue() {
+    }
+
+    public boolean isEmpty() {
+        return head==null;
+    }
+
+    public void enqueue(int x) {
+        Node newNode = new Node(x);
+        if(tail!=null) tail.next = newNode;
+        tail = newNode;
+        if(head==null) head = tail;
+        size++;
+    }
+
+    public void dequeue() {
+        if(head==null) throw new RuntimeException("Queue is empty");
+        int value = head.data;
+        head = head.next;
+        if(head==null) tail = null;
+        size--;
+    }
+
+    public int getFront() {
+        if(isEmpty()) return -1;
+        int value = head.data;
         return value;
-	}
+    }
+
+    public int size() {
+        return size;
+    }
 }
-
-
-
-
