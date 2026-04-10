413953c064546d710de429a9bdc04c79f7e4e477
Yasif khan <125533092+Yasif17@users.noreply.github.com>
2026-04-11 00:48:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,103 +1,65 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] arr = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) arr[idx++] = i;
-
-            new Solution().mergeSort(arr, 0, arr.length - 1);
-
-            for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
-
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-public void merge(int arr[], int l, int m, int r)
-    {
-        // Find sizes of two subarrays to be merged
-        int n1 = m - l + 1;
-        int n2 = r - m;
+    
+    int[] a;
+    int[] tempa;
+    int length;
 
-        // Create temporary arrays
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-
-        // Copy data to temp arrays
-        for (int i = 0; i < n1; ++i)
-            left[i] = arr[l + i];
-        for (int j = 0; j < n2; ++j)
-            right[j] = arr[m + 1 + j];
-
-        // Merge the temp arrays
-
-        int i = 0, j = 0;
-        int k = l;
-
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                arr[k] = left[i];
-                i++;
-            }
-            else {
-                arr[k] = right[j];
-                j++;
-            }
-            k++;
-        }
-
-        // Copy remaining elements of left[], if any
-        while (i < n1) {
-            arr[k] = left[i];
-            i++;
-            k++;
-        }
-
-        // Copy remaining elements of right[], if any
-        while (j < n2) {
-            arr[k] = right[j];
-            j++;
-            k++;
+    void mergeSort(int arr[], int l, int r) {
+        this.a = arr;
+        this.length = arr.length;
+        this.tempa = new int[arr.length];
+        divide(l,r);
+    }
+    
+     void divide(int start,int end){
+        if(start<end){
+            int mid = (start+end)>>>1;
+            
+            divide(start,mid);
+            divide(mid+1,end);
+            
+            merge(start,mid,end);
+            
         }
     }
-
-    public void mergeSort(int arr[], int l, int r)
-    {
-        if (l < r) {
-            // Find the middle point
-            int m = l + (r - l) / 2;
-
-            // Sort first and second halves
-            mergeSort(arr, l, m);
-            mergeSort(arr, m + 1, r);
-
-            // Merge the sorted halves
-            merge(arr, l, m, r);
+        
+         void merge(int start,int mid,int end){
+            for(int i=start;i<=end;i++){
+                tempa[i] = a[i];
+            }
+            
+            int i = start;
+            int j = mid+1;
+            int k =start;
+            
+            while(i<=mid && j<=end){
+                if(tempa[i]<tempa[j]){
+                    a[k] = tempa[i];
+                    i++;
+                }else{
+                    a[k] = tempa[j];
+                    j++;
+                }
+                k++;
+            }
+            while(i<=mid){
+                    a[k] = tempa[i];
+                    i++;
+                    k++;
+                }
+            
         }
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
