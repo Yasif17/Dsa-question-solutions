1e6df7e90b65936a31f415cd49ea0d3ac434db0c
Yasif khan <125533092+Yasif17@users.noreply.github.com>
2026-04-10 23:45:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.java
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -1,66 +1,32 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i) System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-    // Driver program
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String inputLine[] = br.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int arr[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            new Solution().quickSort(arr, 0, n - 1);
-            printArray(arr);
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to sort an array using quick sort algorithm.
-    static void quickSort(int arr[], int low, int high) {
-        // store the partitioned value in the variable
-        int pi = partition(arr,low,high);
+    public void quickSort(int[] arr, int low, int high) {
+
+        int loc = partition(arr,low,high);
         
-        // now check if pi-1 value is bigger then the low index value then recursively call
-        if(low<pi-1){
-            quickSort(arr,low,pi-1);
-        }
-        // again check if pi value is smaller then the high index value then recurively call
-        if(pi<high){
-            quickSort(arr,pi,high);
+        if(low<loc-1){
+            quickSort(arr,low,loc-1);
         }
         
+        if(high>loc){
+            quickSort(arr,loc,high);
+        }
+
     }
 
-    static int partition(int arr[], int low, int high) {
-        // first find pivot element
-        int pivot = arr[(low+high)/2];
+    private int partition(int[] arr, int low, int high) {
         
-        // now sort the array in the left and right perespective
+        int pivot = arr[(low+high)>>>1];
+        
         while(low<=high){
-            // sort the element of the left side of the pivot and then increament
-            while(arr[low]<pivot){
+            
+            while(pivot>arr[low]){
                 low++;
             }
-            //sort the element of the right side of the pivot and then decrement
+            
             while(pivot<arr[high]){
                 high--;
             }
-            // check if low<=high then swap the elements
+            
             if(low<=high){
                 int temp = arr[low];
                 arr[low] = arr[high];
@@ -69,14 +35,7 @@ class Solution {
                 high--;
             }
         }
-        // return the index value of the array
         return low;
+        
     }
 }
-
-
-
-
-
-
-
